6544d7dcff4a6e1a76ad1b9bd4cd0e1dfb1c0f23
rabiebn <rabie.bnmr@gmail.com>
2023-09-07 18:02:29 +0100

    12: re-write

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,11 +9,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t leaves;
 
 	if (!tree)
 		return (0);
 
-	if (!(tree->right) && !(tree->left))
-		return (leaves++);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
 }
