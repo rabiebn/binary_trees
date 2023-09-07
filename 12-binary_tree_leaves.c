92b23f36ab2e3790ea6d13a274ca365d356b7c40
rabiebn <rabie.bnmr@gmail.com>
2023-09-07 17:39:33 +0100

    12: Betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,14 +6,14 @@
  *
  * @tree: root node of the tree
  * Return: number of leaves, 0 if tree is NULL
-*/
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+	size_t leaves = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if (!(tree->right) && !(tree->left))
-        return (leaves++);
+	if (!(tree->right) && !(tree->left))
+		return (leaves++);
 }
